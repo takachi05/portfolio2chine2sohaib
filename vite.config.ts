695b59fcf744ce27742a69da241fea7a5a390eb3
sohaib chine <133054861+takachi05@users.noreply.github.com>
2026-02-11 09:29:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio2chine2sohaib/",
   server: {
     host: "::",
     port: 8080,
